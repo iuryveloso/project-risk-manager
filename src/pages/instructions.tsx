f4ba87ed8f5eb1c2c6e4db11c23afb36054c7109
Iury Veloso <iurysveloso@gmail.com>
2022-11-21 13:25:19 -0300

    Add instructions page && remove anused pages

--- src/pages/instructions.tsx
+++ src/pages/instructions.tsx
@@ -1,4 +1,5 @@
 import Layout from '@components/template/Layout'
+import { faker } from '@faker-js/faker'
 
 export default function Instructions() {
   return (
@@ -7,7 +8,46 @@ export default function Instructions() {
       title={'Instruções do Sistema'}
       subtitle={'Visualize e entenda o funcionamento do sistema'}
     >
-      <span>Instruções</span>
+      <div
+        className={`
+            w-full
+            flex-grow
+            bg-slate-200 dark:bg-slate-700 p-4 
+        `}
+      >
+        <div className={'mb-3'}>
+          <h3 className={'font-bold text-xl text-center'}>Projetos</h3>
+          <div className={'flex flex-col text-lg'}>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+          </div>
+        </div>
+        <div className={'mb-3'}>
+          <h3 className={'font-bold text-xl text-center'}>Riscos</h3>
+          <div className={'flex flex-col text-lg'}>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+          </div>
+        </div>
+        <div className={'mb-3'}>
+          <h3 className={'font-bold text-xl text-center'}>Tarefas</h3>
+          <div className={'flex flex-col text-lg'}>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+          </div>
+        </div>
+        <div className={'mb-3'}>
+          <h3 className={'font-bold text-xl text-center'}>Ações dos Riscos</h3>
+          <div className={'flex flex-col text-lg'}>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+            <div className={'mb-2'}>{faker.lorem.paragraph()}</div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
